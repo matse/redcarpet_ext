827febc65acba5223c15a43cf7ebdb694a4b7e1b
Matthias Seelig <matthias.seelig@ymail.com>
2014-03-27 16:07:51 +0100

    only set title on a link if given one

--- lib/redcarpet_ext.rb
+++ lib/redcarpet_ext.rb
@@ -32,26 +32,32 @@ module RedcarpetExt
     def link(link, title, content)
 
       possible_attributes = ['nofollow', 'blank']
-      html_options = nil
+      html_options = {}
 
       begin
         if nil != (parsed = parse_link(link))
           link = parsed[:url]
-          title = parsed[:title]
+          title = parsed[:title] || title
           unless parsed[:attributes].nil?
             classes = parsed[:attributes] - possible_attributes
             attributes = parsed[:attributes].keep_if{ |x| possible_attributes.include? x }
           end
         end
 
-        html_options = { title: title, class: classes }
+        html_options = { title: title } unless title.empty?
+        html_options = html_options.merge({ class: classes }) unless classes.empty?
+
         if attributes
           html_options = html_options.merge(parse_attributes(attributes))
         end
         link_to(content, link, html_options)
 
       rescue Exception => e
-        link_to(content, link, title: title)
+        if title.empty?
+          link_to(content, link)
+        else
+          link_to(content, link, title: title)
+        end
       end
 
     end
